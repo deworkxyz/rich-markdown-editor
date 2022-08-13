c94b9c2595585f8a9bfe78e933f13aedcf69c1ca
David Fant <david@fant.io>
2022-08-13 22:26:24 +0200

    don't prefix \ for new lines

--- src/nodes/Paragraph.ts
+++ src/nodes/Paragraph.ts
@@ -33,7 +33,7 @@ export default class Paragraph extends Node {
       node.childCount === 0 &&
       !state.inTable
     ) {
-      state.write("\\\n");
+      state.write("\n");
     } else {
       state.renderInline(node);
       state.closeBlock(node);
